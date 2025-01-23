35a2b9798b105624e032403e1ba137cee7fd671d
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-23 19:52:21 +0530

    conditionals

---
+++ ifelse.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 34;
+$b = 0;
+
+// if( $a > $b ){
+//     echo "a is greater than b";
+// }
+// else{
+//     echo "a is not greater than b";
+// }
+$age = 21;
+
+if($age<18){
+    echo "You are a minor.";
+}
+elseif($age>=18){
+    echo "You are an adult.";
+}
+
+
+?>
---
+++ switchCase.php
@@ -0,0 +1,20 @@
+<?php
+
+$age = 67;
+
+switch ($age) {
+    case 12:
+        echo "You are 12 years old";
+        break;
+    case 45:
+        echo "You are 45 years old";
+        break;
+    case 67:
+        echo "You are 63 years old";
+        break;
+    default:
+        echo "Your age is weird";
+        break;
+}
+
+?>
