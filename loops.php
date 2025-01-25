7ddacaa5330305c77e5bff59774843cc25db0ca8
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-25 18:41:33 +0530

    Loops

---
+++ loops.php
@@ -0,0 +1,41 @@
+<?php
+
+
+echo "While Loop: <br>";
+
+// $i=0;
+// while ( $i<=10 ) {
+//     echo "The number is: $i <br>";
+//     $i++;
+// }
+
+
+echo "For Loop: <br>";
+
+
+// for( $i= 0; $i< 5; $i++ ){
+//     echo "The number is: $i <br>";
+// }
+
+
+echo "Do While Loop: <br>";
+// $i = 2;
+// do {
+//     echo "The number is: $i <br>";
+//     $i++ ;
+// }while($i<10);
+
+echo "For Each Loop: <br>";
+
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $color) {
+    echo "The color is: $color <br>";
+}
+
+
+
+
+
+
+?>
